90fb45ab24f94a5a85a89ba90c547fa5a226ee44
Никита Сергеев <sergeev_Nikita89@mail.ru>
2021-06-06 16:20:06 +0300

    функционирует кнопка "Добавить" (не доработано)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,27 @@ namespace CalculationOfSeniority
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.gb_1 = new System.Windows.Forms.GroupBox();
-            this.dtp_1 = new System.Windows.Forms.DateTimePicker();
+            this.DateBegin = new System.Windows.Forms.DateTimePicker();
             this.gb_2 = new System.Windows.Forms.GroupBox();
-            this.dtp_2 = new System.Windows.Forms.DateTimePicker();
+            this.DateEnd = new System.Windows.Forms.DateTimePicker();
             this.Add_button = new System.Windows.Forms.Button();
             this.Delete_button = new System.Windows.Forms.Button();
             this.Print_button = new System.Windows.Forms.Button();
             this.About_button = new System.Windows.Forms.Button();
             this.Close_button = new System.Windows.Forms.Button();
             this.gb_3 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DateBase = new System.Windows.Forms.DataGridView();
             this.gb_1.SuspendLayout();
             this.gb_2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DateBase)).BeginInit();
             this.SuspendLayout();
             // 
             // gb_1
             // 
-            this.gb_1.Controls.Add(this.dtp_1);
+            this.gb_1.Controls.Add(this.DateBegin);
             this.gb_1.Location = new System.Drawing.Point(18, 13);
             this.gb_1.Name = "gb_1";
             this.gb_1.Size = new System.Drawing.Size(144, 62);
@@ -56,17 +57,17 @@ namespace CalculationOfSeniority
             this.gb_1.TabStop = false;
             this.gb_1.Text = "Начало периода:";
             // 
-            // dtp_1
+            // DateBegin
             // 
-            this.dtp_1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtp_1.Location = new System.Drawing.Point(6, 26);
-            this.dtp_1.Name = "dtp_1";
-            this.dtp_1.Size = new System.Drawing.Size(120, 27);
-            this.dtp_1.TabIndex = 0;
+            this.DateBegin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DateBegin.Location = new System.Drawing.Point(6, 26);
+            this.DateBegin.Name = "DateBegin";
+            this.DateBegin.Size = new System.Drawing.Size(120, 27);
+            this.DateBegin.TabIndex = 0;
             // 
             // gb_2
             // 
-            this.gb_2.Controls.Add(this.dtp_2);
+            this.gb_2.Controls.Add(this.DateEnd);
             this.gb_2.Location = new System.Drawing.Point(474, 13);
             this.gb_2.Name = "gb_2";
             this.gb_2.Size = new System.Drawing.Size(144, 62);
@@ -74,13 +75,13 @@ namespace CalculationOfSeniority
             this.gb_2.TabStop = false;
             this.gb_2.Text = "Конец периода:";
             // 
-            // dtp_2
+            // DateEnd
             // 
-            this.dtp_2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtp_2.Location = new System.Drawing.Point(6, 26);
-            this.dtp_2.Name = "dtp_2";
-            this.dtp_2.Size = new System.Drawing.Size(120, 27);
-            this.dtp_2.TabIndex = 0;
+            this.DateEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DateEnd.Location = new System.Drawing.Point(6, 26);
+            this.DateEnd.Name = "DateEnd";
+            this.DateEnd.Size = new System.Drawing.Size(120, 27);
+            this.DateEnd.TabIndex = 0;
             // 
             // Add_button
             // 
@@ -90,6 +91,7 @@ namespace CalculationOfSeniority
             this.Add_button.TabIndex = 3;
             this.Add_button.Text = "Добавить";
             this.Add_button.UseVisualStyleBackColor = true;
+            this.Add_button.Click += new System.EventHandler(this.Add_button_Click);
             // 
             // Delete_button
             // 
@@ -99,6 +101,7 @@ namespace CalculationOfSeniority
             this.Delete_button.TabIndex = 4;
             this.Delete_button.Text = "Удалить";
             this.Delete_button.UseVisualStyleBackColor = true;
+            this.Delete_button.Click += new System.EventHandler(this.Delete_button_Click);
             // 
             // Print_button
             // 
@@ -136,31 +139,39 @@ namespace CalculationOfSeniority
             this.gb_3.TabStop = false;
             this.gb_3.Text = "Итого:";
             // 
-            // dataGridView1
+            // DateBase
             // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DateBase.AllowUserToOrderColumns = true;
+            this.DateBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DateBase.DefaultCellStyle = dataGridViewCellStyle1;
+            this.DateBase.Location = new System.Drawing.Point(18, 82);
+            this.DateBase.Name = "DateBase";
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(18, 82);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(600, 319);
-            this.dataGridView1.TabIndex = 9;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DateBase.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DateBase.RowHeadersWidth = 51;
+            this.DateBase.RowTemplate.Height = 29;
+            this.DateBase.Size = new System.Drawing.Size(600, 319);
+            this.DateBase.TabIndex = 9;
             // 
             // Seniority
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 518);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.DateBase);
             this.Controls.Add(this.gb_3);
             this.Controls.Add(this.Close_button);
             this.Controls.Add(this.About_button);
@@ -174,7 +185,7 @@ namespace CalculationOfSeniority
             this.Text = "Расчет стажа";
             this.gb_1.ResumeLayout(false);
             this.gb_2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DateBase)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -182,15 +193,15 @@ namespace CalculationOfSeniority
         #endregion
         private System.Windows.Forms.GroupBox gb_1;
         private System.Windows.Forms.GroupBox gb_2;
-        private System.Windows.Forms.DateTimePicker dtp_2;
+        private System.Windows.Forms.DateTimePicker DateEnd;
         private System.Windows.Forms.Button Add_button;
         private System.Windows.Forms.Button Delete_button;
         private System.Windows.Forms.Button Print_button;
         private System.Windows.Forms.Button About_button;
         private System.Windows.Forms.Button Close_button;
         private System.Windows.Forms.GroupBox gb_3;
-        private System.Windows.Forms.DateTimePicker dtp_1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker DateBegin;
+        private System.Windows.Forms.DataGridView DateBase;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,28 @@ namespace CalculationOfSeniority
             InitializeComponent();
             ConnectionToDb();
         }
+        private void Add_button_Click(object sender, EventArgs e)
+        {
+            string conStr = @"Data Source=SERGEEV\SERGEEV;Initial Catalog=master;User Id = sa; Password = 123";
+
+            SqlConnection connection = new SqlConnection(conStr);
+
+            connection.Open();
+
+            ////string sql = "select DateBegin as [Дата начала], DateEnd as [Дата конец], WorkExperience as [Стаж] from  Seniority";
+
+            string sql = "INSERT INTO [Seniority] (DateBegin, DateEnd, WorkExperience) VALUES (@DateBegin, @DateEnd, @WorkExperience)";
+
+            SqlCommand createCommand = new SqlCommand(sql, connection);
+
+            createCommand.Parameters.AddWithValue("@DateBegin", DateBegin.Value.ToString()); //или без ToString();
+            createCommand.Parameters.AddWithValue("@DateEnd", DateEnd.Value.ToString()); //или без ToString();
+            createCommand.Parameters.AddWithValue("@WorkExperience", DateEnd.Value.ToString()); //или без ToString();
+
+            createCommand.ExecuteNonQuery();
+
+        }
+
         private void ConnectionToDb()
         {
             string conStr = @"Data Source=SERGEEV\SERGEEV;Initial Catalog=master;User Id = sa; Password = 123";
@@ -33,8 +55,13 @@ namespace CalculationOfSeniority
             SqlDataAdapter dataAdp = new SqlDataAdapter(createCommand);
             DataTable dt = new DataTable("Seniority"); // В скобках указываем название таблицы
             dataAdp.Fill(dt);
-            dataGridView1.DataSource = dt.DefaultView; // Сам вывод 
+            DateBase.DataSource = dt.DefaultView; // Сам вывод 
             connection.Close();
         }
+
+        private void Delete_button_Click(object sender, EventArgs e)
+        {
+        }
     }
+
 }
